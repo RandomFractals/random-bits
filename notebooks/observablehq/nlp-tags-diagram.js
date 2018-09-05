a5829ae880d8c683de1781121a14f524643f3ce3
Taras Novak <taras.novak@randomfractals.com>
2018-09-05 10:02:21 -0500

    started working on new nlp-tags-diagram.js notebook

---
+++ notebooks/observablehq/nlp-tags-diagram.js
@@ -0,0 +1,290 @@
+// URL: https://beta.observablehq.com/@randomfractals/nlp-tags-diagram
+// Title: NLP Tags Diagram
+// Author: Taras Novak (@randomfractals)
+// Version: 38
+// Runtime version: 1
+
+const m0 = {
+  id: "e92eff14ab092b9d@38",
+  variables: [
+    {
+      inputs: ["md"],
+      value: (function(md){return(
+md `# NLP Tags Diagram`
+)})
+    },
+    {
+      inputs: ["md"],
+      value: (function(md){return(
+md `Notebook for displaying NLP tags from 
+[Compromise Tags](https://beta.observablehq.com/@spencermountain/compromise-tags)
+
+...`
+)})
+    },
+    {
+      inputs: ["legends"],
+      value: (function(legends){return(
+legends
+)})
+    },
+    {
+      inputs: ["tagTypes"],
+      value: (function(tagTypes){return(
+tagTypes
+)})
+    },
+    {
+      inputs: ["tagColors"],
+      value: (function(tagColors){return(
+tagColors
+)})
+    },
+    {
+      name: "imports",
+      inputs: ["md"],
+      value: (function(md){return(
+md `## Imports`
+)})
+    },
+    {
+      name: "nlp",
+      inputs: ["require"],
+      value: (function(require){return(
+require('compromise')
+)})
+    },
+    {
+      from: "@spencermountain/nlp-compromise",
+      name: "printList",
+      remote: "printList"
+    },
+    {
+      from: "@spencermountain/nlp-compromise",
+      name: "printHtml",
+      remote: "printHtml"
+    },
+    {
+      from: "@randomfractals/hello-nlp",
+      name: "tagTypes",
+      remote: "tagTypes"
+    },
+    {
+      from: "@randomfractals/hello-nlp",
+      name: "tagColors",
+      remote: "tagColors"
+    },
+    {
+      from: "@randomfractals/hello-nlp",
+      name: "legends",
+      remote: "legends"
+    },
+    {
+      from: "@randomfractals/hello-nlp",
+      name: "toWords",
+      remote: "toWords"
+    },
+    {
+      from: "@randomfractals/hello-nlp",
+      name: "toShortList",
+      remote: "toShortList"
+    },
+    {
+      name: "styles",
+      inputs: ["md"],
+      value: (function(md){return(
+md `## Styles`
+)})
+    },
+    {
+      name: "nlpStyles",
+      inputs: ["html"],
+      value: (function(html){return(
+html `
+<style>
+.scrollable-container {
+  max-height: 400px;
+  overflow: auto;
+}
+.short-list {
+  max-height: 200px;
+}
+.big{
+  font-size:1.5rem;
+  color:cornflowerblue;
+}
+.small{
+  color:grey;
+  margin-top:30px;
+}
+.term { color:grey; cursor:pointer;}
+.nl-Person { border-bottom:2px solid #6393b9; }
+.nl-Pronoun { border-bottom:2px solid #81acce; }
+.nl-Plural { border-bottom:2px solid steelblue; }
+.nl-Singular { border-bottom:2px solid lightsteelblue; }
+.nl-Verb { border-bottom:2px solid palevioletred; }
+.nl-Adverb { border-bottom:2px solid #f39c73; }
+.nl-Adjective { border-bottom:2px solid #b3d3c6; }
+.nl-Determiner { border-bottom:2px solid #d3c0b3; }
+.nl-Preposition { border-bottom:2px solid #9794a8; }
+.nl-Conjunction { border-bottom:2px solid #c8c9cf; }
+.nl-Value { border-bottom:2px solid palegoldenrod; }
+.nl-QuestionWord { border-bottom:2px solid lavender; }
+.nl-Acronym { border-bottom:2px solid violet; }
+.nl-Possessive { border-bottom:2px solid #7990d6; }
+.nl-Noun { border-bottom:2px solid #7990d6; }
+.nl-Expression { border-bottom:2px solid #b3d3c6; }
+.nl-Negative { border-bottom:2px solid #b4adad; }
+</style>
+`
+)})
+    },
+    {
+      name: "references",
+      inputs: ["md"],
+      value: (function(md){return(
+md `## References
+
+- [Compromise NLP Intro](https://beta.observablehq.com/@spencermountain/nlp-compromise)
+- [Compromise NLP Normalize](https://beta.observablehq.com/@spencermountain/compromise-normalization)
+- [Compromise NLP Topics](https://beta.observablehq.com/@spencermountain/topics-named-entity-recognition)
+- [Compromise Tags](https://beta.observablehq.com/@spencermountain/compromise-tags)
+`
+)})
+    }
+  ]
+};
+
+const m1 = {
+  id: "@spencermountain/nlp-compromise",
+  variables: [
+    {
+      name: "printList",
+      value: (function()
+{ 
+  const max = 35
+  return (list) => {
+     let len=list.length
+     list=list.slice(0, max)
+     let el = document.createElement("table");
+     el.innerHTML = list.reduce((str, o)=>{
+       str += '<tr>'
+       str += `<td style="color:#46468B;">${o.normal || o.text || ''}</td>`
+       str += `<td style="color:#7A7A8B;">${o.count || ''}</td>`
+       str += `<td style="color:#B7B7D1;">${o.percent+ '%'}</td>`
+        str += '</tr>'
+       return str
+     },'')
+     if(len>list.length){
+       el.innerHTML+='<b>(of '+len+' results)<b>'
+     }
+     return el
+   } 
+}
+)
+    },
+    {
+      name: "printHtml",
+      inputs: ["DOM"],
+      value: (function(DOM){return(
+function printHtml(doc){
+  let el = DOM.element()
+  let html = doc.out('html')
+  el.innerHTML = html
+  //add a hover 'title'
+  let sentences= el.children[0].children
+  for (var i = 0; i < sentences.length; i++) {
+    sentences[i].style='display:block;'
+    for (var o = 0; o < sentences[i].children.length; o++) {
+      let e=sentences[i].children[o]
+      var tags = e.getAttribute('class').split(' ').map(c=>c.replace(/^nl-/,' '))
+      e.classList.add('term')
+      e.setAttribute('title', tags)
+    }
+  }
+  return el
+}
+)})
+    }
+  ]
+};
+
+const m2 = {
+  id: "@randomfractals/hello-nlp",
+  variables: [
+    {
+      name: "tagTypes",
+      value: (function(){return(
+[
+  'Expression',
+  'Pronoun',
+  'Noun',
+  'Verb',
+  'Adjective',
+  'Adverb',
+  'Conjunction',
+  'Preposition',
+  'Determiner',
+  'QuestionWord',
+  'Value',  
+]
+)})
+    },
+    {
+      name: "tagColors",
+      value: (function(){return(
+{
+  Pronoun: '#81acce',
+  Verb: 'palevioletred',
+  Adverb: '#f39c73',
+  Adjective: '#b3d3c6',
+  Determiner: '#d3c0b3',
+  Preposition: '#9794a8',
+  Conjunction: '#c8c9cf',
+  QuestionWord: 'lavender',
+  Noun: '#7990d6',
+  Expression: '#b3d3c6',
+  Value: 'palegoldenrod',  
+}
+)})
+    },
+    {
+      name: "legends",
+      inputs: ["html","tagTypes"],
+      value: (function(html,tagTypes){return(
+html `<p class="term">
+  ${tagTypes.map(type => `<span class="nl-${type}" title="${type}">${type}</span> `)
+    .reduce((html, tag) => html + tag)}
+</p>`
+)})
+    },
+    {
+      name: "toWords",
+      value: (function(){return(
+function toWords (terms) {
+  return terms.map(term => ({
+    text: term.normal,
+    count: term.count,
+    freq: term.percent/100
+  }));
+}
+)})
+    },
+    {
+      name: "toShortList",
+      inputs: ["html"],
+      value: (function(html){return(
+function toShortList(list) {
+  return html `<div class="scrollable-container short-list">${list}</div>`;
+}
+)})
+    }
+  ]
+};
+
+const notebook = {
+  id: "e92eff14ab092b9d@38",
+  modules: [m0,m1,m2]
+};
+
+export default notebook;
