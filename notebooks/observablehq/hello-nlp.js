b78eabdb39d3ac08e0e28fd0146484a48a746ff9
Taras Novak <taras.novak@randomfractals.com>
2018-09-06 08:56:28 -0500

    moved nlp tag tree to new notebook
    updated other nlp notebooks to use it via import

--- notebooks/observablehq/hello-nlp.js
+++ notebooks/observablehq/hello-nlp.js
@@ -1,11 +1,11 @@
 // URL: https://beta.observablehq.com/@randomfractals/hello-nlp
 // Title: Hello, NLP!
 // Author: Taras Novak (@randomfractals)
-// Version: 562
+// Version: 573
 // Runtime version: 1
 
 const m0 = {
-  id: "c2ff228e09d0a4ae@562",
+  id: "c2ff228e09d0a4ae@573",
   variables: [
     {
       inputs: ["md"],
@@ -170,70 +170,15 @@ md `## Lingo Parts`
 )})
     },
     {
-      name: "lingoTree",
-      inputs: ["tree","lingo","d3","DOM","width"],
-      value: (function(tree,lingo,d3,DOM,width)
-{
-  const root = tree(lingo);
-  let x0 = Infinity;
-  let x1 = -x0;
-  root.each(d => {
-    if (d.x > x1) x1 = d.x;
-    if (d.x < x0) x0 = d.x;
-  });
-  const svg = d3.select(DOM.svg(width, x1 - x0 + root.dx * 2))
-    .style("width", "100%")
-    .style("height", "auto");
-  
-  const g = svg.append("g")
-    .attr("font-family", "sans-serif")
-    .attr("font-size", 10)
-    .attr("transform", `translate(${root.dy / 3},${root.dx - x0})`);
-    
-  const link = g.append("g")
-    .attr("fill", "none")
-    .attr("stroke", "#555")
-    .attr("stroke-opacity", 0.4)
-    .attr("stroke-width", 1.5)
-  .selectAll("path")
-    .data(root.links())
-    .enter().append("path")
-      .attr("d", d3.linkHorizontal()
-        .x(d => d.y)
-        .y(d => d.x));
-  
-  const node = g.append("g")
-    .attr("stroke-linejoin", "round")
-    .attr("stroke-width", 3)
-    .selectAll("g")
-    .data(root.descendants().reverse())
-    .enter().append("g")
-      .attr("transform", d => `translate(${d.y},${d.x})`);
-
-  node.append("circle")
-    .attr("fill", d => d.children ? "#555" : d.data.color) //"#999")
-    .attr("r", 2.5);
-
-  node.append("text")
-    .attr("dy", "0.31em")
-    .attr("x", d => d.children ? -6 : 6)
-    .attr("text-anchor", d => d.children ? "end" : "start")
-    .attr("fill", d => d.data.color)
-    .text(d => d.data.count ? `${d.data.count} ${d.data.name}`: `${d.data.children.length} ${d.data.name}s`)
-    .clone(true).lower()
-    .attr("stroke", "white");
-  
-  return svg.node();
-}
-)
+      inputs: ["lingoTree"],
+      value: (function(lingoTree){return(
+lingoTree
+)})
     },
     {
-      inputs: ["html","DOM","rasterize","lingoTree","serialize"],
-      value: (async function(html,DOM,rasterize,lingoTree,serialize){return(
-html`
-${DOM.download(await rasterize(lingoTree), `lyrics-lingo-tree.png`, "Download as PNG")}
-${DOM.download(await serialize(lingoTree), `lyrics-lingo-tree.svg`, "Download as SVG")}
-`
+      inputs: ["downloadLingoTree"],
+      value: (function(downloadLingoTree){return(
+downloadLingoTree
 )})
     },
     {
@@ -241,18 +186,6 @@ ${DOM.download(await serialize(lingoTree), `lyrics-lingo-tree.svg`, "Download as
       inputs: ["tagTree"],
       value: (function(tagTree){return(
 tagTree
-)})
-    },
-    {
-      name: "tree",
-      inputs: ["d3","lingo","width"],
-      value: (function(d3,lingo,width){return(
-data => {
-  const root = d3.hierarchy(lingo);
-  root.dx = 10;
-  root.dy = width / (root.height + 1);
-  return d3.tree().nodeSize([root.dx, root.dy])(root);
-}
 )})
     },
     {
@@ -334,90 +267,34 @@ doc.contractions().data()
 )})
     },
     {
-      name: "tagTypes",
-      value: (function(){return(
-[
-  'Expression',
-  'Pronoun',
-  'Noun',
-  'Verb',
-  'Adjective',
-  'Adverb',
-  'Conjunction',
-  'Preposition',
-  'Determiner',
-  'QuestionWord',
-  'Value',  
-]
+      inputs: ["tagTypes"],
+      value: (function(tagTypes){return(
+tagTypes
 )})
     },
     {
-      name: "tagColors",
-      value: (function(){return(
-{
-  Pronoun: '#81acce',
-  Verb: 'palevioletred',
-  Adverb: '#f39c73',
-  Adjective: '#b3d3c6',
-  Determiner: '#d3c0b3',
-  Preposition: '#9794a8',
-  Conjunction: '#c8c9cf',
-  QuestionWord: 'lavender',
-  Noun: '#7990d6',
-  Expression: '#b3d3c6',
-  Value: 'palegoldenrod',  
-}
+      inputs: ["tagColors"],
+      value: (function(tagColors){return(
+tagColors
 )})
     },
     {
-      name: "tags",
-      inputs: ["doc"],
-      value: (function(doc){return(
-doc.out('tags')
-)})
-    },
-    {
-      name: "uniqueTags",
       inputs: ["tags"],
-      value: (function(tags)
-{
-  const map = new Map();
-  for (const tag of tags) {
-    let group = map.get(tag.normal);
-    if (!group) {
-      group = {name: tag.normal, children: []};
-      map.set(tag.normal, group);
-    }
-    group.children.push(tag);
-    tag.targets = [];
-  }
-  return {name: 'tags', children: [...map.values()]};
-}
-)
+      value: (function(tags){return(
+tags
+)})
     },
     {
-      name: "tagTree",
-      inputs: ["tagTypes","uniqueTags","tagColors"],
-      value: (function(tagTypes,uniqueTags,tagColors)
-{
-  const map = new Map();
-  for (const tagType of tagTypes) {
-    map.set(tagType, {name: tagType, children: []});
-  }
-  for (const tag of uniqueTags.children) {
-    const tagTypes = tag.children[0].tags;
-    for (const tagType of tagTypes) {
-      const type = map.get(tagType);
-      if (type) {
-        type.children.push({name: tag.name, count: tag.children.length, color: tagColors[tagType]});
-        type.color = tagColors[tagType];
-        break;
-      }
-    }
-  }
-  return {name: 'term', children: [...map.values()]};
-}
-)
+      inputs: ["uniqueTags"],
+      value: (function(uniqueTags){return(
+uniqueTags
+)})
+    },
+    {
+      inputs: ["tagTree"],
+      value: (function(tagTree){return(
+tagTree
+)})
     },
     {
       inputs: ["md"],
@@ -630,14 +507,44 @@ require('d3-cloud')
       remote: "fontSize"
     },
     {
-      from: "@mbostock/saving-svg",
-      name: "rasterize",
-      remote: "rasterize"
+      from: "@randomfractals/nlp-tag-tree",
+      name: "lingoTree",
+      remote: "lingoTree"
     },
     {
-      from: "@mbostock/saving-svg",
-      name: "serialize",
-      remote: "serialize"
+      from: "@randomfractals/nlp-tag-tree",
+      name: "downloadLingoTree",
+      remote: "downloadLingoTree"
+    },
+    {
+      from: "@randomfractals/nlp-tag-tree",
+      name: "tree",
+      remote: "tree"
+    },
+    {
+      from: "@randomfractals/nlp-tag-tree",
+      name: "tags",
+      remote: "tags"
+    },
+    {
+      from: "@randomfractals/nlp-tag-tree",
+      name: "uniqueTags",
+      remote: "uniqueTags"
+    },
+    {
+      from: "@randomfractals/nlp-tag-tree",
+      name: "tagTree",
+      remote: "tagTree"
+    },
+    {
+      from: "@randomfractals/nlp-tag-tree",
+      name: "tagTypes",
+      remote: "tagTypes"
+    },
+    {
+      from: "@randomfractals/nlp-tag-tree",
+      name: "tagColors",
+      remote: "tagColors"
     },
     {
       inputs: ["md"],
@@ -1191,9 +1098,355 @@ function rasterize(svg) {
   ]
 };
 
+const m4 = {
+  id: "@randomfractals/nlp-tag-tree",
+  variables: [
+    {
+      name: "lingoTree",
+      inputs: ["tree","lingo","d3","DOM","width"],
+      value: (function(tree,lingo,d3,DOM,width)
+{
+  const root = tree(lingo);
+  let x0 = Infinity;
+  let x1 = -x0;
+  root.each(d => {
+    if (d.x > x1) x1 = d.x;
+    if (d.x < x0) x0 = d.x;
+  });
+  const svg = d3.select(DOM.svg(width, x1 - x0 + root.dx * 2))
+    .style("width", "100%")
+    .style("height", "auto");
+  
+  const g = svg.append("g")
+    .attr("font-family", "sans-serif")
+    .attr("font-size", 10)
+    .attr("transform", `translate(${root.dy / 3},${root.dx - x0})`);
+    
+  const link = g.append("g")
+    .attr("fill", "none")
+    .attr("stroke", "#555")
+    .attr("stroke-opacity", 0.4)
+    .attr("stroke-width", 1.5)
+  .selectAll("path")
+    .data(root.links())
+    .enter().append("path")
+      .attr("d", d3.linkHorizontal()
+        .x(d => d.y)
+        .y(d => d.x));
+  
+  const node = g.append("g")
+    .attr("stroke-linejoin", "round")
+    .attr("stroke-width", 3)
+    .selectAll("g")
+    .data(root.descendants().reverse())
+    .enter().append("g")
+      .attr("transform", d => `translate(${d.y},${d.x})`);
+
+  node.append("circle")
+    .attr("fill", d => d.children ? "#555" : d.data.color) //"#999")
+    .attr("r", 2.5);
+
+  node.append("text")
+    .attr("dy", "0.31em")
+    .attr("x", d => d.children ? -6 : 6)
+    .attr("text-anchor", d => d.children ? "end" : "start")
+    .attr("fill", d => d.data.color)
+    .text(d => d.data.count ? `${d.data.count} ${d.data.name}`: `${d.data.children.length} ${d.data.name}s`)
+    .clone(true).lower()
+    .attr("stroke", "white");
+  
+  return svg.node();
+}
+)
+    },
+    {
+      name: "downloadLingoTree",
+      inputs: ["html","DOM","rasterize","lingoTree","serialize"],
+      value: (async function(html,DOM,rasterize,lingoTree,serialize){return(
+html`
+${DOM.download(await rasterize(lingoTree), `lingo-tree.png`, "Download as PNG")}
+${DOM.download(await serialize(lingoTree), `lingo-tree.svg`, "Download as SVG")}
+`
+)})
+    },
+    {
+      name: "tree",
+      inputs: ["d3","lingo","width"],
+      value: (function(d3,lingo,width){return(
+data => {
+  const root = d3.hierarchy(lingo);
+  root.dx = 10;
+  root.dy = width / (root.height + 1);
+  return d3.tree().nodeSize([root.dx, root.dy])(root);
+}
+)})
+    },
+    {
+      name: "tags",
+      inputs: ["doc"],
+      value: (function(doc){return(
+doc.out('tags')
+)})
+    },
+    {
+      name: "uniqueTags",
+      inputs: ["tags"],
+      value: (function(tags)
+{
+  const map = new Map();
+  for (const tag of tags) {
+    let group = map.get(tag.normal);
+    if (!group) {
+      group = {name: tag.normal, children: []};
+      map.set(tag.normal, group);
+    }
+    group.children.push(tag);
+    tag.targets = [];
+  }
+  return {name: 'tags', children: [...map.values()]};
+}
+)
+    },
+    {
+      name: "tagTree",
+      inputs: ["tagTypes","uniqueTags","tagColors"],
+      value: (function(tagTypes,uniqueTags,tagColors)
+{
+  const map = new Map();
+  for (const tagType of tagTypes) {
+    map.set(tagType, {name: tagType, children: []});
+  }
+  for (const tag of uniqueTags.children) {
+    const tagTypes = tag.children[0].tags;
+    for (const tagType of tagTypes) {
+      const type = map.get(tagType);
+      if (type) {
+        type.children.push({name: tag.name, count: tag.children.length, color: tagColors[tagType]});
+        type.color = tagColors[tagType];
+        break;
+      }
+    }
+  }
+  return {name: 'term', children: [...map.values()]};
+}
+)
+    },
+    {
+      name: "tagTypes",
+      value: (function(){return(
+[
+  'Expression',
+  'Pronoun',
+  'Noun',
+  'Verb',
+  'Adjective',
+  'Adverb',
+  'Conjunction',
+  'Preposition',
+  'Determiner',
+  'QuestionWord',
+  'Value',  
+]
+)})
+    },
+    {
+      name: "tagColors",
+      value: (function(){return(
+{
+  Pronoun: '#81acce',
+  Verb: 'palevioletred',
+  Adverb: '#f39c73',
+  Adjective: '#b3d3c6',
+  Determiner: '#d3c0b3',
+  Preposition: '#9794a8',
+  Conjunction: '#c8c9cf',
+  QuestionWord: 'lavender',
+  Noun: '#7990d6',
+  Expression: '#b3d3c6',
+  Value: 'palegoldenrod',  
+}
+)})
+    },
+    {
+      name: "lingo",
+      inputs: ["tagTree"],
+      value: (function(tagTree){return(
+tagTree
+)})
+    },
+    {
+      name: "d3",
+      inputs: ["require"],
+      value: (function(require){return(
+require('d3')
+)})
+    },
+    {
+      from: "@mbostock/saving-svg",
+      name: "rasterize",
+      remote: "rasterize"
+    },
+    {
+      from: "@mbostock/saving-svg",
+      name: "serialize",
+      remote: "serialize"
+    },
+    {
+      name: "doc",
+      inputs: ["nlp","text"],
+      value: (function(nlp,text){return(
+nlp(text.value).normalize({
+  whitespace: true, // remove hyphens, newlines, and force one space between words
+  punctuation: true, // remove commas, semicolons - but keep sentence-ending punctuation
+  case: true, // keep only first-word, and 'entity' titlecasing
+  numbers: true, // 'one'  →  '1'
+  plurals: true, // 'eyes'  →  'eye'  
+  verbs: true, // 'swtiched' → 'switch'
+})
+)})
+    },
+    {
+      name: "nlp",
+      inputs: ["require"],
+      value: (function(require){return(
+require('compromise')
+)})
+    },
+    {
+      name: "text",
+      inputs: ["html"],
+      value: (function(html){return(
+html `<textarea rows="10" cols="60">
+[Verse 1: Killer Mike]
+Hear what I say, we are the business today
+Fuck shit is finished today (what)
+RT and J—we the new PB & J
+We dropped a classic today (what)
+We did a tablet of acid today
+Lit joints with the matches and ashes away
+SKRRRT! We dash away
+Donner and Dixon, the pistol is blasting away
+
+[Verse 2: El-P]
+Doctors of death
+Curing our patients of breath
+We are the pain you can trust
+Crooked at work
+Cookin' up curses and slurs
+Smokin' my brain into mush
+I became famous for flamin' you fucks
+Maimin' my way through the brush
+There is no training or taming of me and my bruh
+Look like a man, but I'm animal raw
+
+[Verse 3: Killer Mike]
+We are the murderous pair
+That went to jail and we murdered the murderers there
+Then went to Hell and discovered the devil
+Delivered some hurt and despair
+Used to have powder to push
+Now I smoke pounds of the kush
+Holy, I'm burnin' a bush
+Now I give a fuck about none of this shit
+Jewel runner over and out of this bitch
+
+[Hook]
+Woo!
+Woo!
+Step into the spotlight, woo!
+Woo!
+
+[Verse 4: El-P]
+Copping of uppers and downers get done
+I'm in a rush to be numb
+Droppin' a thousand ain't much
+Come from the clouds
+On a missile to turn this whole town into dust
+Don't make a sound, baby, hush
+I am the living swipe right, on the mic I'm a slut
+I don't know how to not spit like a lout
+I'll spill a pound of my kids on your couch
+
+[Verse 5: Killer Mike]
+Half of a mongrel and mythical team
+Villainous treacherous things
+Legend says El is a spawn out of Hell
+The myth is my mama's a murderous queen
+Your life can end like in Godfather 1
+You get the gun as I christen my son
+If I die today and it's Hell I should pay
+Tell the Lord Mikey said, "Fuck, it was fun"
+
+[Verse 6: El-P]
+Every new record's my dick in a box
+We get a doozy, the moola's a lock
+You're getting used to me doing no wrong
+I don't play chicken, you prick, I'm a fox
+You wanna kick it, I'll give you the rocks
+You kiss the wood chipper blade if you bark
+I'm fuckin' magic, in fact I'm a warlock of talk
+I got a unicorn horn for a (stop)
+
+[Hook]
+Woo!
+Woo!
+Step into the spotlight, woo!
+Woo!
+
+[Interlude]
+And the crowd goes RTJ!
+And the crowd goes RTJ!
+And the crowd goes RTJ!
+And the crowd goes RTJ!
+RTJ!
+RTJ!
+RTJ!
+RTJ!
+
+[Verse 7: Killer Mike]
+Mike Pentangeli won't snitch
+I'll rent a room at the Ritz
+I'll sip a fifth of the whisk
+I'll smoke a dub in the tub
+Then I will split both my wrists
+
+[Verse 8: El-P]
+I'll pull a sword on you simps
+Just with a flick of the wrist
+Get your neck giving up mist
+Me and Mike skip away whistlin' and grin
+Every day's golden when you only win
+
+[Verse 9: Killer Mike & El-P]
+Bullyin' bastards and beatin' on beats
+Sounds like a day at the beach, preach
+I keep it middle school, step on your feet
+Before you can speak, blaow! to the teeth
+
+[Verse 10: El-P]
+We move among the ones you think are meek
+You think I'm lion, you right, see my teeth
+Don't be a bore when I'm roaring vamoose
+Hunting's no fun when your prey doesn't move
+I'll put a gun to a bunny like choose
+Say somethin' funny or bunny go boom
+You got a bevy of shit you could groove
+We'd like to thank you for choosing our crew
+
+[Verse 11: Killer Mike]
+And that's from the crew you can trust
+Warranty plus for fuckin' shit up
+We are the no-gooders, do-gooders
+Known to the dancers and dealers and doers of dust
+</textarea>`
+)})
+    }
+  ]
+};
+
 const notebook = {
-  id: "c2ff228e09d0a4ae@562",
-  modules: [m0,m1,m2,m3]
+  id: "c2ff228e09d0a4ae@573",
+  modules: [m0,m1,m2,m3,m4]
 };
 
 export default notebook;
