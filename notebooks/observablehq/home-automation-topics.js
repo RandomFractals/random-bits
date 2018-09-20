91a4161b5ea4eaa6ff2131d6691c184d393e96c9
Taras Novak <taras.novak@randomfractals.com>
2018-09-20 17:18:20 -0500

    added new home automation topics js notebook starter

---
+++ notebooks/observablehq/home-automation-topics.js
@@ -0,0 +1,50 @@
+// URL: https://beta.observablehq.com/@randomfractals/home-automation-topics
+// Title: Home Automation Topics
+// Author: Taras Novak (@randomfractals)
+// Version: 29
+// Runtime version: 1
+
+const m0 = {
+  id: "b70b41ed345acbfa@29",
+  variables: [
+    {
+      inputs: ["md"],
+      value: (function(md){return(
+md`# Home Automation Topics
+
+Home automation topics mind map TBD (to be developed :)`
+)})
+    },
+    {
+      inputs: ["html"],
+      value: (function(html){return(
+html `<a href="https://beta.observablehq.com/search?query=Chicago%20Crimes">
+  <img src="https://raw.githubusercontent.com/RandomFractals/random-bits/master/notebooks/observablehq/HomeAutomationMap.jpg" alt="Home Automation DAG" />
+</a>
+`
+)})
+    },
+    {
+      inputs: ["md"],
+      value: (function(md){return(
+md `**will graph [this awesome home automation brain dump sketch](https://twitter.com/Unix_Guru/status/1039102816416133120) for my dear remote Canadian friend Michael Ball (aka [Unix_Guru](https://twitter.com/Unix_Guru)) this week since he's been a great supporter of my [Chicago Crimes EDA](https://beta.observablehq.com/collection/@randomfractals/chicago-crimes) IRL dataViz endeavours and I am into that home automation alerts shit too!** :)
+
+can probably easily rework this graphviz with .dot syntax and cover that spread with diff root nodes coloring similar to this bit:
+
+https://beta.observablehq.com/@randomfractals/notebooks#notebooksGraph
+
+what good remote dev freinds for!
+
+Mike, just give me another day or 2. Still shaping up other things on this and other flames fronts ;)
+`
+)})
+    }
+  ]
+};
+
+const notebook = {
+  id: "b70b41ed345acbfa@29",
+  modules: [m0]
+};
+
+export default notebook;
