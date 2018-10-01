0b8eb1c72922480a9531ce88588ece3db9622475
Taras Novak <taras.novak@randomfractals.com>
2018-10-01 05:59:04 -0500

    started working on new bookmarks.js prototype notebook

---
+++ notebooks/observablehq/bookmarks.js
@@ -0,0 +1,33 @@
+// URL: https://beta.observablehq.com/@randomfractals/bookmarks
+// Title: Bookmarks
+// Author: Taras Novak (@randomfractals)
+// Version: 6
+// Runtime version: 1
+
+const m0 = {
+  id: "a6509fe4c567a490@6",
+  variables: [
+    {
+      inputs: ["md"],
+      value: (function(md){return(
+md `# Bookmarks
+
+Liked notebooks bookmarker.
+
+### Basic UX design Idea:
+
+- show liked notebook list
+- load liked notebook cells code on notebook list item click
+- add 🔖 toggle to the notebook cell to bookmark it
+- use local storage for 📚's with 🔖's save and display ... 🤗`
+)})
+    }
+  ]
+};
+
+const notebook = {
+  id: "a6509fe4c567a490@6",
+  modules: [m0]
+};
+
+export default notebook;
