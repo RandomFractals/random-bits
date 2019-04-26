d2bb0bfd3e8f47100e56d70e603ae29c5dca563e
Taras Novak <taras.novak@randomfractals.com>
2019-04-26 14:44:57 -0500

    added new data-inspector.js Observable notebook backup start :)

---
+++ notebooks/observablehq/data-inspector.js
@@ -0,0 +1,34 @@
+// URL: https://observablehq.com/@randomfractals/data-inspector
+// Title: Data Inspector
+// Author: Taras Novak (@randomfractals)
+// Version: 10
+// Runtime version: 1
+
+const m0 = {
+  id: "08772c01decd03fb@10",
+  variables: [
+    {
+      inputs: ["md"],
+      value: (function(md){return(
+md `# Data Inspector
+
+TBD (to be developed ;)`
+)})
+    },
+    {
+      inputs: ["md"],
+      value: (function(md){return(
+md `## Dev Log
+
+see [#dataInspector 🕵️‍♀️ on Twitter](https://twitter.com/hashtag/dataInspector?src=hash)`
+)})
+    }
+  ]
+};
+
+const notebook = {
+  id: "08772c01decd03fb@10",
+  modules: [m0]
+};
+
+export default notebook;
